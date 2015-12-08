016bd55f113a19d7459eba62738c8182c81efd56
egeniesse <egeniesse@gmail.com>
2015-12-07 17:34:25 -0800

    almost done with the spec runner

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,3 +1,11 @@
+
+
+function escapeHtml(str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+};
+
 var app = {
     //
     server: 'https://api.parse.com/1/classes/chatterbox',
@@ -31,10 +39,14 @@ var app = {
         success: function (data) {
           var $chat = $('#chats');
           for (var i = 0; i < data.results.length; i++){
-            var user = "<div class = 'user'><p>" + data.results[i].username + "</p></div>";
-            var text = "<div class = 'text'><p>" + data.results[i].text + "</p></div>";
-            var time = "<div class = 'time'><p>" + data.results[i].createdAt + "</p></div>";
-            $chat.append('<div class = "message">' + user + time + text + '</div>')
+            var sanitizedRoom = escapeHtml(data.results[i].roomname);
+            //var dataRoom = sanitizedRoom.split(' ').join('_')
+            var dataRoom = sanitizedRoom.replace(/\s/g, '_');
+            var user = "<div class = 'username' data-username = " + data.results[i].username + "><p>" + escapeHtml(data.results[i].username) + "</p></div>";
+            var text = "<div class = 'text'><p>" + escapeHtml(data.results[i].text) + "</p></div>";
+            var time = "<div class = 'time'><p>" + escapeHtml(data.results[i].createdAt) + "</p></div>";
+            var room = "<div class = 'time'><p>" + sanitizedRoom + "</p></div>";
+            $chat.append('<div class = "message" data-roomname='+dataRoom+'>' + user + time + text + room +'</div>')
           }
           // var test = '<div>' + data.results[0] + '<div>';
           //append to the dom
@@ -54,18 +66,44 @@ var app = {
     addMessage: function(message) {
       var $chat = $('#chats');
       
-        var user = "<div class = 'user'><p>" + message.username + "</p></div>";
+        var user = "<div class = 'username'><p>" + message.username + "</p></div>";
         var text = "<div class = 'text'><p>" + message.text + "</p></div>";
-        var time = "<div class = 'time'><p>" + message.roomname + "</p></div>";
-        $chat.append('<div class = "message">' + user + time + text + '</div>')
+        var room = "<div class = 'time'><p>" + message.roomname + "</p></div>";
+        $chat.append('<div class = "message">' + user + room + text + '</div>')
       },
 
     addRoom: function(roomname) {
-      $('#roomSelect').append('<div>' + roomname + '</div>');
-    }
+      $('#roomSelect').append('<div class = "room">' + roomname + '</div>');
+    }, 
 
-    // $('document').ready(function(){
+    addFriend : function(name){
+      $.ajax({
+        url: this.server,
+        type: 'POST',
+        data: JSON.stringify(name),
+        success: function(data){
+          console.log('Friend added')
+        }, 
+        error : function(data){
+          console.log(data)
+        }
+      })
+    },
 
-    // });
 
-}
+};
+
+$('document').ready(function(){
+  $('body').on('click', '.username', function(){ app.addFriend($(this).data('username')) })
+
+  $('.submit').on('click', function() {
+    var message = {};
+    message.time = new Date();
+    message.username = 'Frank';
+    message.text = $('.messageBody').val();
+    console.log(message)
+    app.send(message);
+  });
+});
+
+app.fetch();
